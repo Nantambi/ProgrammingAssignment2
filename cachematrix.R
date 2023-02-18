3330e9f1bdcbfcf7f8cbb15332c356c62a04b402
Nantambi <58690868+Nantambi@users.noreply.github.com>
2023-02-18 13:43:38 -0800

    Update cachematrix.R
    Submitted work

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,28 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+makecachedmatrix-creates an object to hold a matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+        cachedinverse<-function(newx){
+                x<<-newx
+                cachedinverse<<-NULL
+}
+        get<-function()x
+        setcachedinverse<-function(inverse)cachedinverse<<-inverse
+        getcachedinverse<-function()cachedinverse
+        list(set=set,get=get,setcachedinverse=setcachedinverse,getcachedinverse=getcachedinverse)
 }
 
 
 ## Write a short comment describing this function
-
+cachedsolve-returns the inverse of the matrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+       cached<-x$getcachedinverse()
+        if(!is.null(cached)){
+                return(cached)
+        }
+        z<-x$get()
+        cached<-solve(z)
+        x$setcachedinverse(cached)
+        cached
 }
